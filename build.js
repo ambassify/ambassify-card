4fd3c49beba42e6eb0b72b68c93d0738dad8d3b4
Wim Mostmans <wim@sitebase.be>
2022-12-23 14:01:57 +0100

    create endpoints for each person

--- build.js
+++ build.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const pug = require('pug');
+const data = require('./data.json');
+console.log(data.persons);
 
 // Compile the source code
 const compiledFunction = pug.compileFile('test.html');
 
-const content = compiledFunction({
-    name: 'Timothy',
-    address: {
-        zip: 2440
+data.persons.forEach((person) => {
+    const content = compiledFunction({
+        name: person.firstName,
+        email: person.email
+    });
+
+    const dir = 'dist/' + person.firstName.toLowerCase() + '/';
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
     }
+
+    fs.writeFileSync(dir + 'index.html', content);
 });
-fs.writeFileSync('dist/bla.html', content);
